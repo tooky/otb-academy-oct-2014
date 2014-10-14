d87355714d2c2f355eec75a79e85cee1768b0d85
Asta Bevainyte <asta.bevainyte@onthebeach.co.uk>
2014-10-14 11:51:22 +0100

    fake class

--- tyre-pressure/spec/alarm_spec.rb
+++ tyre-pressure/spec/alarm_spec.rb
@@ -34,11 +34,27 @@ RSpec.describe "tyre pressure alarm" do
 
     let(:sample_pressure){ 10 }
     
-    it 'should be on' do 
+    it 'should be on and off' do 
       alarm.check
       expect(alarm.on?).to be true
       # sensor pressure is now normal????
       allow(sensor).to receive(:sample_pressure).and_return(20)
+      alarm.check
+      expect(alarm.on?).to be false
+    end
+  end
+  context "creating fake sensor" do
+   class FakeSensor
+     attr_accessor :sample_pressure
+   
+    end 
+    let(:sensor){FakeSensor.new }
+    it 'should be on and off' do 
+      sensor.sample_pressure = 10
+      alarm.check
+      expect(alarm.on?).to be true
+      # sensor pressure is now normal????
+      sensor.sample_pressure = 19
       alarm.check
       expect(alarm.on?).to be false
     end
