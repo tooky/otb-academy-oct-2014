26b8af1da2706f134a33d3efad97dd6367508c0a
Steve Tooke <steve@boxjump.co.uk>
2014-10-20 21:14:49 +0100

    Create first test for in memory product list

---
+++ checkout/lib/checkout.rb
@@ -0,0 +1,9 @@
+class InMemoryProductList
+  def add_product(product)
+
+  end
+
+  def find_product(barcode)
+
+  end
+end
---
+++ checkout/spec/in_memory_product_list_spec.rb
@@ -0,0 +1,15 @@
+require 'checkout'
+
+class Product < Struct.new(:barcode, :price)
+end
+
+RSpec.describe InMemoryProductList do
+  it "can find a product by it's barcode" do
+    product_list = InMemoryProductList.new
+
+    product = Product.new('abc1234', 10.99)
+    product_list.add_product product
+
+    expect(product_list.find_item('abc1234')).to eq(product)
+  end
+end
